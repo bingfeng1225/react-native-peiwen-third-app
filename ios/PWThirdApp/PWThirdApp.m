298f180435031962c36877ecc20b328092d0588c
peiwen.liu <peiwen.liu@picovr.com>
2018-10-26 17:24:13 +0800

    add ios navigation/payment/phone call method

--- ios/PWThirdApp/PWThirdApp.m
+++ ios/PWThirdApp/PWThirdApp.m
@@ -26,14 +26,52 @@ RCT_EXPORT_MODULE();
 }
 
 RCT_EXPORT_METHOD(phonecall:(NSString *)phone) {
-   
+    NSString *urlsting = [NSString stringWithFormat:@"telprompt:%@",phone];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlsting]];
 }
 
 RCT_EXPORT_METHOD(navigation:(NSString *)destination) {
+    if([self gaodeNavigation:destination]){
+        return;
+    }
+    if([self baiduNavigation:destination]){
+        return;
+    }
+}
+
+- (BOOL)gaodeNavigation:(NSString *)destination{
+    if (![[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"iosamap://"]]){
+        return NO;
+    }
     
+    NSString *urlsting = [[NSString stringWithFormat:@"iosamap://path?sourceApplication=智慧助理&sid=BGVIS1&sname=%@&did=BGVIS2&dname=%@&dev=0&t=0",@"我的位置",destination] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlsting]];
+    return YES;
+}
+
+- (BOOL)baiduNavigation:(NSString *)destination{
+    if (![[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@"baidumap://map/"]]){
+        return NO;
+    }
+    NSString *urlsting = [[NSString stringWithFormat:@"baidumap://map/direction?origin={{我的位置}}&destination=%@&mode=driving",destination] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlsting]];
+    return YES;
 }
 
 RCT_EXPORT_METHOD(payment) {
-    
+    if([self alipayPayment]){
+        return;
+    }
 }
+
+- (BOOL)alipayPayment{
+    if (![[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@"alipay://"]]){
+        return NO;
+    }
+    NSString *urlsting = @"alipay://platformapi/startapp?saId=10000007";
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlsting]];
+    return YES;
+}
+
 @end
